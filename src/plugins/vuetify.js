b5779b778633ec1af8159719c993d6ba465336be
Abir Bouhriz Daidj <bouhrizabir2015@gmail.com>
2021-01-09 22:26:12 +0100

    material icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,9 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: { dark: true,
+    },
+    icons: {
+        iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+      },
 });
